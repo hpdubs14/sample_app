885b6276e301011a992aa74517395200f87dca54
Paul W <zigloo99@gmail.com>
2011-06-15 14:10:56 -0500

    Added a stub User show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   
-  get "users/new"
+  resources :users
 
   root :to => "pages#home"
   
@@ -8,5 +8,6 @@ SampleApp::Application.routes.draw do
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
+  
   
 end
